fb97b24f0998d63d02f54d1930c3d04bc2ebf0f0
ameekaziz <ameek.aziz@gmail.com>
2016-03-10 01:18:01 +0600

    Updated the shell file
    need to update the Readme file

--- start_Ubuntu_VM.sh
+++ start_Ubuntu_VM.sh
@@ -1,16 +1,16 @@
 # # ## automating the VM staritng and Log in
 ###linux 4.4.3-201.fc22.x86_64
-#_________________________setting UP for the first time__________________________#
-# #
+###_________________________setting UP for the first time__________________________####
+# #//un_comment the shell commands or copy them
 # # ##1.setting up the VM for the first time!
 # # # step 1:
 # # # creating ssh key
 # # ssh-keygen
-# # #give everything defult and make the procedure done
+# # #give everything defult and make the procedure done...
 # #
 # # ls .ssh/id_rsa
 # #  # step 2:
-# # #sudo virsh create /etc/libvirt/qemu/VM_name.xml
+# # #sudo virsh create /etc/libvirt/qemu/VM_name.xml // this will run the Virtual machine 
 # # sudo virsh create /etc/libvirt/qemu/ubuntu14.04.xml
 # #
 # # # ssh-copy-id -i ~/.ssh/id_dsa.pub user@<your vm er ip>
@@ -24,13 +24,16 @@
 # #
 #
 
-#____________________________________________regular login____________________________________________#
+###____________________________________________regular login____________________________________________###
 # ##starting my ubuntu14.04 server VM
 # # 1. virsh create n
 # # 2. ssh -i .ssh/id_rsa user@ip
 # #sudo virsh create /etc/libvirt/qemu/VM_name.xml
 sudo virsh create /etc/libvirt/qemu/ubuntu14.04.xml
-#
+
+##to start the VM giving it time!
+sleep 10
+
 # # for getting logged on to the VM
 # # ssh -i .ssh/id_rsa user@ip
 ssh -i ~/.ssh/id_rsa amk@192.168.122.58
